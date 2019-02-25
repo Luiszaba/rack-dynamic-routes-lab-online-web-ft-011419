c789099f8b17a8758c59d3c8550eb031ef865bee
Luis Zabala <luis.zabala076@gmail.com>
2019-02-25 03:49:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,31 +1,24 @@
 class Application
-  
-
-  
   def call(env)
-    
-      @@items = [
- Item.new("Figs",3.42),
- Item.new("Pears",0.99)
-            ]
-            
-    resp = Rack::Response.new
-    req =  Rack::Response.new(env)
-    
+    @@items =[Item.new("Figs",3.42),Item.new("Pears",0.99)]
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+
+
       if req.path.match(/items/)
-        dynamic = req.path.split("/items/").last
-      if  g=@@items.detect { |i|i.name == dynamic}
-        resp.write "You requested the #{g.price}"
+        dynamic =req.path.split("/items/").last
+      if  s=@@items.detect { |i|i.name == dynamic}
+        resp.write "You requested the #{s.price}"
       else
-        resp.status = 400
-        resp.write "Item not found"
-      end
-        else 
+      resp.status = 400
+      resp.write "Item not found"
+    end
+      else
         resp.status = 404
         resp.write "Route not found"
       end
-    resp.finish
-  end
+       resp.finish
 end
 
+end
       
